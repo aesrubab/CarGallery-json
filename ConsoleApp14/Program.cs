6f24e646404813869e6a5c7f2a78fdc7f8b42b11
aesrubab <rubabhuseynova013@gmail.com>
2024-07-15 00:25:48 +0400

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using ConsoleApp14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CarGallery gallery = new CarGallery { Name = "Rubab's Gallery" };
+
+        gallery.AddCar(new Car { Id = 1, Marka = "Bmw", Model = "x7", Year = 2024 });
+        gallery.AddCar(new Car { Id = 2, Marka = "Bmw", Model = "x5", Year = 2023 });
+
+        gallery.GetAllCars();
+
+        gallery.SaveToJson("cars.json");
+
+        Console.WriteLine(" saved to cars.json");
+    }
+}
